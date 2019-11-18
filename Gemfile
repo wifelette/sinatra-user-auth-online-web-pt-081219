3f78f10540cfa8920e16a0c476e73053b00df897
wifelette <LeahSilber@gmail.com>
2019-11-18 15:09:58 -0800

    Done.

--- Gemfile
+++ Gemfile
@@ -1,20 +1,23 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'sinatra'
-gem 'activerecord', '4.2', :require => 'active_record'
-gem 'sinatra-activerecord'
+gem 'activerecord', '5.1', require: 'active_record'
+gem 'activesupport', '5.1', require: 'active_support'
+
+gem 'pry'
 gem 'rake'
 gem 'require_all'
+gem 'shotgun'
+gem 'sinatra'
+gem 'sinatra-activerecord'
 gem 'sqlite3', '~>1.3.6'
 gem 'thin'
-gem 'shotgun'
-gem 'pry'
+gem 'learn-co'
 
 group :test do
-  gem 'launchy'
-  gem 'rspec'
   gem 'capybara'
-  gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+  gem 'launchy'
+  gem 'rack-test'
+  gem 'rspec'
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, proc { File.join(root, "../views/") }
 
   configure do
     enable :sessions
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
@@ -25,7 +24,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
@@ -45,7 +43,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
